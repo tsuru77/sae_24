fe1d8f01624199b3a1b52a645b76465409a38096
tsuru77 <57574188+tsuru77@users.noreply.github.com>
2022-06-03 21:42:12 +0200

    window

--- public/vortex.py
+++ public/vortex.py
@@ -35,7 +35,7 @@ y = 400
 
 for i in range(14):
   for l in range(traits):   #min 10 max 150
-    pygame.draw.line(window, (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)), [400 + 1200 * math.cos(x + 600), 400 + 1200 * math.sin(y + 600)], [400 + cercle1_rayon * math.cos(x + angle), 400 +cercle1_rayon * math.sin(y + angle)], 1)
+    pygame.draw.line(surface, (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)), [400 + 1200 * math.cos(x + 600), 400 + 1200 * math.sin(y + 600)], [400 + cercle1_rayon * math.cos(x + angle), 400 +cercle1_rayon * math.sin(y + angle)], 1)
     pygame.display.flip()
     x += 0.005
     y += 0.005
